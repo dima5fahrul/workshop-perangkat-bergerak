1d92c1a0e7d4a586f1a33ef38cc8ecf548e5c7aa
dima5fahrul <dfpa.05@gmail.com>
2023-03-02 09:23:40 +0700

    Praktikum 2: Dart Basic

--- lib/main.dart
+++ lib/main.dart
@@ -11,17 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Hello World',
+      title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -49,59 +40,69 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 1;
-  String _text = 'ganjil';
+  String _text = 'Ganjil';
 
   void _incrementCounter() {
     setState(() {
+      // Incrementing counter
       _counter++;
-      if (_counter > 10) {
+      if (_counter > 20) {
         _counter = 1;
+      }
+
+      // Menampilkan bilangan ganjil
+      // _text = 'Ganjil: ';
+      // for (int i = 0; i <= _counter; i++) {
+      //   if (i % 2 == 0) {
+      //     _text += '$i, ';
+      //   }
+      // }
+
+      // Menampilkan bilangan genap kelipatan 3
+      // _text = 'Genap: ';
+      // for (int i = 0; i <= _counter; i += 3) {
+      //   if (i % 2 == 0) {
+      //     _text += '$i, ';
+      //   }
+      // }
+
+      // Menampilkan bilangan prima
+      _text = 'Prima: ';
+      for (int i = 0; i <= _counter; i++) {
+        if (i == 1 || i == 2) {
+          _text += '$i, ';
+        } else if (i % 2 != 0) {
+          _text += '$i, ';
+        }
       }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Hello World',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      body: Container(
+        margin: const EdgeInsets.all(20),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'Hello World',
+              ),
+              Text(
+                '$_counter',
+                style: Theme.of(context).textTheme.headline4,
+              ),
+              Text(
+                '$_text',
+                style: Theme.of(context).textTheme.headline4,
+              ),
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
